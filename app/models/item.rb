db935e95adf473c760693b97724668ffe280f751
EC2 Default User <ec2-user@ip-172-31-27-110.us-east-2.compute.internal>
2023-10-17 13:15:29 +0000

    Initial Commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     has_many :cart_items
-    has_one_attached :image
+    has_one_attached :item_image
   def with_tax_price
    (price * 1.1).floor
   end
@@ -8,5 +8,12 @@ class Item < ApplicationRecord
   def subtotal
    item.with_tax_price * amount
   end
-
+  
+  def get_item_image(width, height)
+  unless item_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+    item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  item_image.variant(resize_to_limit: [width, height]).processed
+end
 end
